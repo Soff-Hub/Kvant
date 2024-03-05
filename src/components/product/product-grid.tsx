e525f40f8fc482cbad930dd663b3d052f2094f11
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-03-05 13:45:50 +0500

    category filter

--- src/components/product/product-grid.tsx
+++ src/components/product/product-grid.tsx
@@ -1,16 +1,15 @@
-import type { FC } from 'react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useState, type FC, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 import Alert from '@components/ui/alert';
 import Button from '@components/ui/button';
 import ProductCardAlpine from '@components/product/product-cards/product-card';
 import ProductCardLoader from '@components/ui/loaders/product-card-loader';
 import ProductCardList from '@components/product/product-cards/product-list-view';
-import { useProductsQuery } from '@framework/product/get-all-products';
-import { LIMITS } from '@framework/utils/limits';
 import { Product } from '@framework/types';
 import { useTranslation } from 'src/app/i18n/client';
-import useQueryParam from '@utils/use-query-params';
 import { baseURL } from '@framework/utils/http';
+import axios from 'axios';
+import { API_ENDPOINTS } from '@framework/utils/api-endpoints';
 
 interface ProductGridProps {
   lang: string;
@@ -18,36 +17,36 @@ interface ProductGridProps {
   viewAs: boolean;
 }
 
-
 export const ProductGrid: FC<ProductGridProps> = ({
   className = '',
   lang,
   viewAs,
 }) => {
   const { t } = useTranslation(lang, 'common');
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsloading] = useState(true);
   const pathname = usePathname();
-  const { getParams, query } = useQueryParam(pathname ?? '/');
-  const newQuery: any = getParams(
-    // @ts-ignore
-    `${baseURL}${query}`,
-  );
+  const newPath = pathname.split('/').pop();
 
-  
+  async function getAllProducts() {
+    try {
+      setIsloading(true);
+      const response = await axios.get(
+        baseURL + API_ENDPOINTS.FASHION_PRODUCTS,
+      );
+      setData(response.data?.results);
+      setIsloading(false);
+    } catch (error: any) {
+      setError(error);
+      console.error('Error fetching data:', error);
+    }
+  }
 
-  const {
-    isFetching: isLoading,
-    isFetchingNextPage: loadingMore,
-    fetchNextPage,
-    hasNextPage,
-    data,
-    error,
-  } = useProductsQuery({
-    limit: LIMITS.PRODUCTS_LIMITS,
-    // @ts-ignore
-    newQuery,
-  });
-  
-  
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
 
   return (
     <>
@@ -60,52 +59,45 @@ export const ProductGrid: FC<ProductGridProps> = ({
       >
         {error ? (
           <div className="col-span-full">
-            <Alert message={error?.message} />
+            <Alert message={error} />
           </div>
-        ) : isLoading && !data?.pages?.length ? (
-          Array.from({ length: 30 }).map((_, idx) => (
+        ) : isLoading ? (
+          Array.from({ length: 15 }).map((_, idx) => (
             <ProductCardLoader
               key={`product--key-${idx}`}
               uniqueKey={`product--key-${idx}`}
             />
           ))
+        ) : viewAs ? (
+          data?.map((product: Product) => (
+            <ProductCardAlpine
+              key={`product--key-${product.id}`}
+              product={product}
+              lang={lang}
+              variant=""
+            />
+          ))
         ) : (
-          data?.pages?.map((page: any) => {
-            if (viewAs) {
-              return page?.data?.map((product: Product) => (
-                <ProductCardAlpine
-                  key={`product--key-${product.id}`}
-                  product={product}
-                  lang={lang}
-                  variant=""
-                />
-              ));
-            } else {
-              return page?.data?.map((product: Product) => (
-                <ProductCardList
-                  key={`product--key-${product.id}`}
-                  product={product}
-                  lang={lang}
-                />
-              ));
-            }
-          })
+          data?.map((product: Product) => (
+            <ProductCardList
+              key={`product--key-${product.id}`}
+              product={product}
+              lang={lang}
+            />
+          ))
         )}
-        {/* end of error state */}
       </div>
-      {hasNextPage && (
-        <div className="mt-1.5 py-5 text-center bg-white rounded">
-          <Button
-            loading={loadingMore}
-            disabled={loadingMore}
-            onClick={() => fetchNextPage()}
-            className={'w-60 '}
-            variant={'primary'}
-          >
-            {t('button-load-more')}
-          </Button>
-        </div>
-      )}
+      <div className="mt-1.5 py-5 text-center bg-white rounded">
+        <Button
+          loading={isLoading}
+          disabled={isLoading}
+          // onClick={() => fetchNextPage()}
+          className={'w-60 '}
+          variant={'primary'}
+        >
+          {t('button-load-more')}
+        </Button>
+      </div>
     </>
   );
 };
--- src/components/search/category-filter-menu.tsx
+++ src/components/search/category-filter-menu.tsx
@@ -1,19 +1,13 @@
 import { usePathname, useSearchParams } from 'next/navigation';
 import cn from 'classnames';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
-import { useUI } from '@contexts/ui.context';
+
 import { useEffect, useState } from 'react';
 import Image from '@components/ui/image';
 import { useTranslation } from 'src/app/i18n/client';
 import { FaCheck } from 'react-icons/fa';
 import useQueryParam from '@utils/use-query-params';
 
-function checkIsActive(arr: any, item: string) {
-  if (arr.includes(item)) {
-    return true;
-  }
-  return false;
-}
 function CategoryFilterMenuItem({
   className = 'border-t border-border-base first:border-t-0  py-3 xl:py-3.5 2xl:py-2.5 3xl:py-3',
   item,
@@ -23,52 +17,23 @@ function CategoryFilterMenuItem({
   const { t } = useTranslation(lang, 'common');
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const { updateQueryparams } = useQueryParam(pathname ?? '/');
-  const [formState, setFormState] = useState<string[]>([]);
+  const { getParams, updateQueryparams } = useQueryParam(pathname ?? '/');
+  const [formStatechild, setFormStateChild] = useState<any>(null);
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const [query, setQuery] = useState<any>({});
 
-  const isActive =
-    checkIsActive(formState, item.slug) ||
-    item?.children?.some((_item: any) => checkIsActive(formState, _item.slug));
-  const [isOpen, setOpen] = useState<boolean>(isActive);
-  const [subItemAction, setSubItemAction] = useState<boolean>(false);
-  useEffect(() => {
-    setOpen(isActive);
-  }, [isActive]);
   const { slug, title, children: items, icon } = item;
-  const { displaySidebar, closeSidebar } = useUI();
 
-  function toggleCollapse() {
-    setOpen((prevValue) => !prevValue);
-  }
-  const handleChange = () => {
-    setSubItemAction(!subItemAction);
-  };
-
-  const hasQueryKey = searchParams?.get('category');
-
-  useEffect(() => {
-    updateQueryparams('category', formState.toString());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formState]);
-
-  useEffect(() => {
-    setFormState(hasQueryKey?.split(',') ?? []);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hasQueryKey]);
 
   function onClick() {
-    if (Array.isArray(items) && !!items.length) {
-      toggleCollapse();
-    } else {
-      setFormState(
-        formState.includes(slug)
-          ? formState.filter((item) => item !== slug)
-          : [...formState, slug],
-      );
-
-      displaySidebar && closeSidebar();
-    }
+    updateQueryparams('parent', slug);
   }
+
+  function handleChange(currentItem: any) {
+    updateQueryparams('child', currentItem?.slug);
+  }
+
+  // console.log(params);
 
   let expandIcon;
   if (Array.isArray(items) && items.length) {
@@ -79,10 +44,19 @@ function CategoryFilterMenuItem({
     );
   }
 
+  useEffect(() => {
+    const params = getParams();
+    setQuery(params)
+    if (query?.parent === slug) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [query?.parent]);
+
   return (
     <>
       <li
-        onClick={onClick}
         className={cn(
           'flex justify-between items-center transition text-sm ',
           { 'text-brand': isOpen },
@@ -92,9 +66,8 @@ function CategoryFilterMenuItem({
         <button
           className={cn(
             'text-brand-dark  hover:text-brand font-medium flex items-center w-full ltr:text-left rtl:text-right cursor-pointer group gap-2',
-            { 'py-2': depth > 0 },
           )}
-          onClick={handleChange}
+          onClick={onClick}
         >
           {icon && (
             <div className="inline-flex shrink-0 2xl:w-12 2xl:h-12 3xl:w-auto 3xl:h-auto ltr:mr-2.5 rtl:ml-2.5 md:ltr:mr-4 md:rtl:ml-4 2xl:ltr:mr-3 2xl:rtl:ml-3 3xl:ltr:mr-4 3xl:rtl:ml-4">
@@ -107,41 +80,41 @@ function CategoryFilterMenuItem({
               />
             </div>
           )}
-          {depth > 0 && (
-            <span
-              className={`w-[20px] h-[20px] text-[11px] flex items-center justify-center border-2 border-border-four rounded-full transition duration-500 ease-in-out group-hover:border-yellow-100 text-brand-light ${
-                formState.includes(item.slug) &&
-                'border-yellow-100 bg-yellow-100'
-              }`}
-            >
-              {formState.includes(item.slug) && <FaCheck />}
-            </span>
-          )}
           <span className="capitalize py-0.5">{title}</span>
-
           {expandIcon && (
             <span className="ltr:ml-auto rtl:mr-auto">{expandIcon}</span>
           )}
         </button>
       </li>
-      {Array.isArray(items) && isOpen ? (
+
+      {isOpen && (
         <li>
           <ul key="content" className="text-xs pb-4">
             {items?.map((currentItem: any) => {
-              const childDepth = depth + 1;
               return (
-                <CategoryFilterMenuItem
-                  key={`${currentItem.name}${currentItem.slug}`}
-                  item={currentItem}
-                  depth={childDepth}
-                  className="px-0 border-b-0 "
-                  lang={lang}
-                />
+                <button
+                  className={cn(
+                    'text-brand-dark  hover:text-brand font-medium flex items-center w-full ltr:text-left rtl:text-right cursor-pointer group gap-2',
+                  )}
+                  onClick={() => handleChange(currentItem)}
+                >
+                  <span
+                    className={`w-[20px] h-[20px] text-[11px] flex items-center justify-center border-2 border-border-four rounded-full transition duration-500 ease-in-out group-hover:border-yellow-100 text-brand-light ${
+                      formStatechild === currentItem?.id &&
+                      'border-yellow-100 bg-yellow-100'
+                    }`}
+                  >
+                    <FaCheck />
+                  </span>
+                  <span className="capitalize py-0.5">
+                    {currentItem?.title}
+                  </span>
+                </button>
               );
             })}
           </ul>
         </li>
-      ) : null}
+      )}
     </>
   );
 }
