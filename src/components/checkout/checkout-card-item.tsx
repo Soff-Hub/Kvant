7539b3c0d6fd1fb3e84ebc526c69091d043fc0c7
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-03-09 17:11:51 +0500

    credit cart

--- src/components/checkout/checkout-card-item.tsx
+++ src/components/checkout/checkout-card-item.tsx
@@ -3,22 +3,16 @@ import Image from '@components/ui/image';
 import { generateCartItemName } from '@utils/generate-cart-item-name';
 
 export const CheckoutItem: React.FC<{ item: Item }> = ({ item }) => {
-
-
-
-
   return (
     <div className="flex items-center py-4 border-b border-border-base ">
-      <div className="flex w-16 h-16 border rounded-md border-border-base shrink-0">
-        <Image
-          src={item.image ?? '/assets/placeholder/order-product.svg'}
-          alt={item.title}
-          className="rounded-md ltr:mr-5 rtl:ml-5"
-          width={64}
-          height={64}
-          style={{ width: 'auto' }}
-        />
-      </div>
+      <Image
+        src={item.image ?? '/assets/placeholder/order-product.svg'}
+        alt={item.title}
+        className="rounded-md ltr:mr-5 rtl:ml-5"
+        width={64}
+        height={64}
+        style={{ width: '150px' }}
+      />
       <h6 className="font-normal text-15px text-brand-dark ltr:pl-3 rtl:pr-3">
         {generateCartItemName(item.title, item.attributes)}
       </h6>
