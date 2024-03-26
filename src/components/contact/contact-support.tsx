9332e3e14f19bf133f88f69d51c1781ed089f34c
Ozodbek Abdisamatov <abdisamatovozodbek003@gmail.com>
2024-03-26 16:43:21 +0500

    orders map

--- src/components/contact/contact-support.tsx
+++ src/components/contact/contact-support.tsx
@@ -50,39 +50,47 @@ const ContactSupport: FC<Props> = ({ lang }) => {
 
   return (
     isClient && (
-      <div className="mb-0 3xl:ltr:pr-5 3xl:rtl:pl-5">
-        <div className="mx-auto space-y-4 mb-6">
-          {data.map((item: any) => (
-            <div
-              key={`contact--key${item.id}`}
-              className="flex flex-col lg:flex-row max-w-xs lg:max-w-sm xl:pe-7"
-            >
-              <div className="flex-shrink-0 w-14  h-14 border-2 border-border-two p-3 rounded-md">
-                {item.icon}
+      <>
+        <Heading variant="titleLarge" className="mb-5">
+          {t('Отправить нам сообщение')}
+        </Heading>
+        <div className="mb-0 3xl:ltr:pr-5 3xl:rtl:pl-5">
+          <div className="mx-auto space-y-4 mb-6">
+            {data.map((item: any) => (
+              <div
+                key={`contact--key${item.id}`}
+                className="flex flex-col lg:flex-row max-w-xs lg:max-w-sm xl:pe-7"
+              >
+                <div className="flex-shrink-0 w-14  h-14 border-2 border-border-two p-3 rounded-md">
+                  {item.icon}
+                </div>
+                <div className="lg:ps-3 2xl:ps-4 mt-4 lg:mt-0">
+                  <Heading variant="base" className="">
+                    {t(item.name)}
+                  </Heading>
+                  <Text>
+                    <a href="tel:+998555111111">{t(item.description)}</a>
+                  </Text>
+                </div>
               </div>
-              <div className="lg:ps-3 2xl:ps-4 mt-4 lg:mt-0">
-                <Heading variant="base" className="">
-                  {t(item.name)}
-                </Heading>
-                <Text><a href="tel:+998555111111">{t(item.description)}</a></Text>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <Text className="xl:leading-8">
+            {mounted && (
+              <>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                mattis, orci ac elementum convallis, dolor enim volutpat sapien,
+                id porttitor eros risus convallis nibh. Nullam dui diam, semper
+                sed neque ut, fringilla rutrum velit. Nulla facilisi. Integer
+                suscipit, dolor sed auctor interdum, libero neque fermentum
+                arcu, in dapibus mauris nisl convallis ante. Orci varius natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus.
+              </>
+            )}
+          </Text>
         </div>
-        <Text className="xl:leading-8">
-          {mounted && (
-            <>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              mattis, orci ac elementum convallis, dolor enim volutpat sapien,
-              id porttitor eros risus convallis nibh. Nullam dui diam, semper
-              sed neque ut, fringilla rutrum velit. Nulla facilisi. Integer
-              suscipit, dolor sed auctor interdum, libero neque fermentum arcu,
-              in dapibus mauris nisl convallis ante. Orci varius natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </>
-          )}
-        </Text>
-      </div>
+      </>
     )
   );
 };
